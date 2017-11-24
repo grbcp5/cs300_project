faa9a70cc42d370c68c6b938a0f32db1a0f10ab2
grbcp5 <grbcp5@mst.edu>
2017-11-24 13:42:41 -0600

    Filter incorrect usage

--- src/edu/mst/grbcp5/Main.java
+++ src/edu/mst/grbcp5/Main.java
@@ -1,11 +1,74 @@
 package edu.mst.grbcp5;
 
+import java.util.Scanner;
+
 public class Main {
 
   public static void main( String[] args ) {
 
-    System.out.println( "Hello, world!" );
+    /* Filter out incorrect command line args */
+    if( !filterIncorrectUsage( args ) ) {
+      return;
+    }
 
+    System.out.println( "Main" );
+
+  }
+
+  private static boolean filterIncorrectUsage( String[] args ) {
+
+    if ( args.length < 1 ) {
+      displayUsage();
+
+      return false;
+    }
+
+    if ( args[ 0 ].toLowerCase().contains( "help" ) ) {
+
+      if ( args.length < 2 ) {
+        displayUsage();
+
+        return false;
+      } else {
+
+        if( !displayHelp( args[ 1 ] ) ) {
+          return false;
+        } else {
+          return true;
+        }
+
+      }
+
+    }
+
+    if( !"server_master_standalone_sa".contains( args[ 0 ] ) ) {
+      displayUsage();
+      return false;
+    }
+
+    return true;
+  }
+
+  private static void displayUsage() {
+    System.out.println( "Usage: command [type] <options>\n" );
+
+    System.out.println( "Types:" );
+    System.out.println( "\tserver \t\t( s  )" );
+    System.out.println( "\tmaster \t\t( m  )" );
+    System.out.println( "\tstandAlone \t( sa )\n" );
+
+    System.out.println( "Use \"command help [type]\" to view options help" );
+  }
+
+  private static boolean displayHelp( String type ) {
+
+    Scanner input = new Scanner( System.in );
+
+    System.out.println( "help" );
+
+    System.out.print( "Would you like to continue with default values: " );
+
+    return input.next().toLowerCase().contains( "y" );
   }
 
 }
